824e0bb81e75ee786af6914710dfd991921a9d30
Filippo Greppi <145936486+unibo-FilippoGreppi@users.noreply.github.com>
2024-12-02 20:25:01 +0100

    ...

--- 112-stream-utilities-for-text-processing/src/main/java/it/unibo/oop/lab/streams/LambdaFilter.java
+++ 112-stream-utilities-for-text-processing/src/main/java/it/unibo/oop/lab/streams/LambdaFilter.java
@@ -6,8 +6,9 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.LayoutManager;
 import java.awt.Toolkit;
+import java.util.Arrays;
 import java.util.function.Function;
-
+import java.util.stream.Collectors;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -33,12 +34,29 @@ import javax.swing.JTextArea;
 public final class LambdaFilter extends JFrame {
 
     private static final long serialVersionUID = 1760990730218643730L;
+    private static final String ANY_NON_WORD = "(\\s|\\p{Punct})+";
 
     private enum Command {
         /**
          * Commands.
          */
-        IDENTITY("No modifications", Function.identity());
+        IDENTITY("No modifications", Function.identity()),
+        TO_LOWER_CASE("String to lower case", String::toLowerCase),
+        COUNT_CHARS("Count number of chars", str -> String.valueOf(str.length())),
+        COUNT_LINES("Count number of lines", str -> Long.toString(str.chars().filter(e -> e == '\n').count() + 1)),
+        LIST_IN_ALFA_ORDER("Words in alphabetical order", str -> 
+            Arrays.stream(str.split(ANY_NON_WORD))
+                .sorted()
+                .collect(Collectors.joining("\n"))
+        ),
+        COUNT_EACH_WORD("Count for each word", str ->
+            Arrays.stream(str.split(ANY_NON_WORD))
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .map(e -> e.getKey() + " -> " + e.getValue())
+                .collect(Collectors.joining("\n"))
+        );
 
         private final String commandName;
         private final Function<String, String> fun;
@@ -60,7 +78,7 @@ public final class LambdaFilter extends JFrame {
 
     private LambdaFilter() {
         super("Lambda filter GUI");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
         final JPanel panel1 = new JPanel();
         final LayoutManager layout = new BorderLayout();
         panel1.setLayout(layout);
